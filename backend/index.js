2b0a195dc240b0bf5f713b8a9e1f612567defd02
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-07-10 21:54:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,4 @@ app.get("/get-file", (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`app listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
